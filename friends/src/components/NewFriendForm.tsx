c3d7d19a0a704804cd24813a33ca679bb5ca7f48
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-27 17:00:37 -0500

    Adds post ability, need to listen for response (I guess)

--- friends/src/components/NewFriendForm.tsx
+++ friends/src/components/NewFriendForm.tsx
@@ -1,3 +1,18 @@
-import React from "react"
+import React, { FormEventHandler } from "react"
+import axios from "axios"
 
-export default () => <p>Placeholder</p>
+export default () => {
+  const handleClick: FormEventHandler = event => {
+    event.preventDefault()
+    axios.post("http://10.0.0.53:5000/friends", {
+      name: "Person",
+      age: 72,
+      email: "user@email.com"
+    })
+  }
+  return (
+    <form>
+      <button onClick={handleClick}>Add new friend</button>
+    </form>
+  )
+}
